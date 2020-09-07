cb9fe4dc13fff48c71bd6850f03e3d16e72d446a
Edward George <edward.george@maersk.com>
2020-09-07 20:44:10 +0200

    refactor to subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::path::Path;
 
 mod hasher;
@@ -8,20 +8,29 @@ use hasher::*;
 fn main() -> std::result::Result<(), anyhow::Error> {
     env_logger::init();
     let matches = App::new("Build Helper")
-        .arg(
-            Arg::with_name("directory")
-                .required(false)
-                .index(1)
-                .default_value("."),
-        )
-        .arg(
-            Arg::with_name("pretty-print")
-                .short("-p")
-                .required(false)
-                .takes_value(false),
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("hash-components")
+                .about("Annotate components.json with hashes")
+                .arg(
+                    Arg::with_name("directory")
+                        .required(false)
+                        .index(1)
+                        .default_value("."),
+                )
+                .arg(
+                    Arg::with_name("pretty-print")
+                        .short("-p")
+                        .required(false)
+                        .takes_value(false),
+                ),
         )
         .get_matches();
-    let p: &Path = matches.value_of_os("directory").unwrap().as_ref();
-    let path = p.canonicalize()?;
-    run_hasher(&path, matches.is_present("pretty-print"))
+    if let Some(m) = matches.subcommand_matches("hash-components") {
+        let p: &Path = m.value_of_os("directory").unwrap().as_ref();
+        let path = p.canonicalize()?;
+        run_hasher(&path, m.is_present("pretty-print"))
+    } else {
+        panic!("unexpected subcommand")
+    }
 }
